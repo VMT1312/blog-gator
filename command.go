b7826d1d4bb7415cae306611d1c8651fb837ed9b
VMT1312 <thiemmavinh123@gmail.com>
2025-07-08 17:30:14 +0800

    Add handlerGetUsers function to retrieve and display all users

--- command.go
+++ command.go
@@ -89,3 +89,20 @@ func handlerRestes(s *state, cmd command) error {
 	fmt.Println("Table has been reset successfully")
 	return nil
 }
+
+func handlerGetUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+
+	return nil
+}
